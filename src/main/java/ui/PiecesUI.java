f5cb880bd94135e6d5b751ff989c920362a4379d
th3-rocha <murilorocha537@gmail.com>
2022-11-30 22:45:30 -0300

    feat: implemented printboard

--- src/main/java/ui/PiecesUI.java
+++ src/main/java/ui/PiecesUI.java
@@ -1,5 +1,28 @@
 package ui;
 
-public class PiecesUI {
+import javax.swing.*;
+import javax.swing.JPanel;
+import java.awt.*;
 
+public class PiecesUI {
+    PiecesUI(){
+
+
+
+    }
+    public JLabel Pawn() {
+        JLabel Pawn = new JLabel("P");
+        //ImageIcon wPawn =  new ImageIcon("src/Art/wPawn.png");
+        //label.setIcon(wPawn);
+        Pawn.setFont(new Font("Italic", Font.BOLD, 70));
+
+        /*
+        Houses[1][1].add(new JLabel("P"));
+        Houses[3][1].add(P1);
+        Houses[2][1].add(P2);
+        frame.setVisible(true);
+        */
+        return Pawn;
+
+    }
 }
