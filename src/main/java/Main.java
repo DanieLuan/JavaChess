d69a9768b40405af33e131e342b52f9340c719b8
th3-rocha <murilorocha537@gmail.com>
2022-12-04 21:52:47 -0300

    update: variable name in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,15 +12,11 @@ public class Main {
 
     public static void main(String[] args) {
         Board board = new Board();
-        BoardUI BoardGui = new BoardUI(board);
+        BoardUI Board = new BoardUI(board);
         PieceUI PieceGui = new PieceUI();
 
-        BoardGui.PrintBoard();
-        BoardGui.spawnPiecesStarter();
-
-
-        //JLabel pawnWhite = PieceGui.Pawn(Color.WHITE);
-        //JLabel pawnBlack = PieceGui.Pawn(Color.BLACK);
+        Board.PrintBoard();
+        Board.spawnPiecesStarter();
 
 
 
